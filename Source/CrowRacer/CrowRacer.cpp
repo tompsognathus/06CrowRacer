9bcede6617ab2e39f8d49d18d26703bf6ca746d2
tompsognathus <tompsognathus@gmail.com>
2023-07-22 21:29:14 +0100

    Basic setup (gitignore, added assets, saved map)

---
+++ Source/CrowRacer/CrowRacer.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CrowRacer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CrowRacer, "CrowRacer" );
---
+++ Source/CrowRacer/CrowRacerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CrowRacerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CROWRACER_API ACrowRacerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
